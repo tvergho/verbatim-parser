34bd6ef9f3a146e7cbdfce5d03f2ed9c11240d83
Tyler Vergho <tvergho@gmail.com>
2021-11-26 11:00:31 -0800

    feat: scrape main page for school links

--- scraper.py
+++ scraper.py
@@ -4,8 +4,8 @@ from bs4 import BeautifulSoup
 wiki_url = "https://opencaselist.paperlessdebate.com/"
 
 class Scraper:
-  def __init__(self):
-    main_page = requests.get(wiki_url)
+  def __init__(self, url):
+    main_page = requests.get(url)
     contents = main_page.content
     self.main_soup = BeautifulSoup(contents, "html.parser")
     self.school_links = list(filter(lambda el : el.name == "a", map(lambda el : el.contents[0], self.main_soup.find_all("span", class_="wikilink"))))
@@ -14,4 +14,4 @@ class Scraper:
     print(self.school_links)
 
 if __name__ == "__main__":
-  scraper = Scraper()
+  scraper = Scraper(wiki_url)
